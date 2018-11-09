6cb2ad6b1bf0365c947cede526d8a8dae0662711
Projjol Banerji <probaner23@gmail.com>
2018-11-09 19:01:27 +0530

    Update decoder for new mode

--- pyint/decoder.py
+++ pyint/decoder.py
@@ -1,9 +1,11 @@
-def decode(arr):
+def decode(arr, input_type = "int"):
     """Takes an array of varint converted numbers, returns original number"""
     if not arr:
         raise ValueError("Could not decode varint, empty values passed")
     decoded_val = 0
     shift = 0
+    if input_type == "hex":
+        arr = [int(ele,16) for ele in arr]
     for val in arr:
         #remove msb
         decoded_val += (val & 0x7f) << shift
