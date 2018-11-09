4e152ddbc783225cb1f0ea553a2f1db2a984423c
Projjol Banerji <probaner23@gmail.com>
2018-11-09 19:05:38 +0530

    bump version 1.0.0 -> 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyint",
-    version="1.0.0",
+    version="1.1.0",
     author="Projjol Banerji",
     author_email="ping@projjol.me",
     description="Python implementation of protobuf style varints",
